eff81249d232ffeaea249cff6c8e4785a7f5f9de
Arthur <malachevski@gmail.com>
2016-01-19 15:31:02 +0700

    Update ex.rb

--- ex.rb
+++ ex.rb
@@ -2,14 +2,28 @@ require "rubygems"
 require "test/unit"
 require "watir-webdriver"
 require "page-object"
+
 class Header
   include PageObject
   link(:sign_up_button, :xpath => '//a[@href="/auth/register"]')
+  link(:logout_button, :xpath => '//a[@href="/auth/logout"]')
+  link(:login_button, :xpath => '//a[@href="/auth/login"]')
 
   def click_signup_link
     sign_up_button
     SignUp.new(@browser)
   end
+
+  def click_login_link
+    login_button
+    LogIn.new(@browser)
+  end
+
+  def click_logout_link
+    logout_button
+    LogIn.new(@browser)
+  end
+
 end
 class Categoriez
   include PageObject
@@ -40,7 +54,7 @@ class ListBooks
     book_block = addable_books_elements[random_index]
     book_name = book_block.h2(:xpath => '//div[@class="product-title"]/h2')
     book_block.link(:xpath => '//a[contains(@class,"button  add-to-cart")]').click
-    book_name
+    #book_name
   end
 
   def is_addable_books_exist
@@ -50,6 +64,30 @@ class ListBooks
 
 end
 
+ class LogIn
+    include PageObject
+
+    text_field(:login_name, :name => "user_name")
+    text_field(:login_password, :name => "password")
+    
+
+    def fill_form_login(login_name, login_password)
+      self.login_name = login_name
+      self.login_password = login_password
+    end
+
+    def click_login_link
+      login_button
+    end
+
+    def fill_form_login_and_login (login_name, login_password)
+      self.login_name = login_name
+      self.login_password = login_password
+      click_login_link
+    end
+end
+
+
 class SignUp
   include PageObject
   text_field(:first, :name => "account[first_name]")
@@ -57,7 +95,8 @@ class SignUp
   text_field(:email, :name => "account[email]")
   text_field(:pass, :name => "account[password]")
   text_field(:repass, :name => "account[password_confirmation]")
-  button(:sign_up_button, :xpath => '//*[@id="newUser"]/div[4]/input')
+  button(:complete_signup, :xpath => '//form[@id="newUser"]//input[@name="commit"]')
+
 
   def fill_form(first, last, email, pass, repass)
     self.first = first
@@ -68,7 +107,7 @@ class SignUp
   end
 
   def click_signup
-    sign_up_button
+    complete_signup
     SignUp.new(@browser)
   end
 
@@ -76,6 +115,12 @@ class SignUp
     self.fill_form(first, last, email, pass, repass)
     self.click_signup
   end
+
+  def logout_after_signup
+    logout_button
+  end
+
+
 end
 class SignupTest < Test::Unit::TestCase
   def setup
@@ -88,35 +133,19 @@ class SignupTest < Test::Unit::TestCase
     @browser.close
   end
 
-  # def test_01_registration
-  #   link = Links.new(@browser)
-  #   home = link.main_page
-  #   signup = link.signup_link
-  #   user_data = signup.sign_up("random_name","random_lastname","1a123@gmail.ru","qwerty","qwerty")
-  # end
-
-  # def test_02_signup
-  #   link = Links.new(@browser)
-  #   home = link.main_page
-  #   login = link.login_link
-  #   user_data = login.log_in("1a123@gmail.ru", "qwerty")
-  # end
-
-  def test_03_category
-    def add_random_book_to_cart(cat_page)
-
-    end
+  def test_full_cycle
     @browser.goto("http://retail.circlesoft.net/")
-    #sign_up_page = Header.new(@browser).click_signup_link
-    #sign_up_page.fill_form_and_sign_up("d", "d", "gs@gs.gs", "df", "df")
+    sign_up_page = Header.new(@browser).click_signup_link
+    sign_up_page.fill_form_and_sign_up("d", "d", "gas@gs.gs", "df", "df")
     cat_page = Categoriez.new(@browser)
     def add_random_book(cat_page)
+
       book_list_page  = cat_page.click_random_category
       while not book_list_page.is_addable_books_exist do
         @browser.back
         add_random_book(cat_page)
       end
-      name = book_list_page.add_to_cart_random_book.text
+      name = book_list_page.add_to_cart_random_book
       name
     end
     books = []
